fe21a8c63e460bcc66ffe29051ade995b89bafd4
jxdwinter <jxdwinter@gmail.com>
2014-05-29 17:53:21 +0800

    init commit

---
+++ ZhihuDaily/ZHDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZHDAppDelegate.h
+//  ZhihuDaily
+//
+//  Created by JiangXiaodong on 5/29/14.
+//  Copyright (c) 2014 jxdwinter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZhihuDaily/ZHDViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZHDViewController.h
+//  ZhihuDaily
+//
+//  Created by JiangXiaodong on 5/29/14.
+//  Copyright (c) 2014 jxdwinter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHDViewController : UIViewController
+
+@end
